00de64beb9e2e50815edbcbc2a7bbafe6befd23d
amzac <amaury.bauzac@gmail.com>
2016-10-02 21:17:15 +0200

    First templates twig and routing

---
+++ src/Blogger/BlogBundle/Controller/PageController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Blogger\BlogBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response as Response;
+
+class PageController extends Controller
+{
+    // routing http://symfony.com/doc/current/bundles/SensioFrameworkExtraBundle/annotations/routing.html
+
+
+    /**
+     * @return Response
+     * @Route(path="/", name="homepage")
+     */
+    public function indexAction()
+    {
+        return $this->render('@BloggerBlog/Page/index.html.twig');
+    }
+
+    /**
+     * @Route("/about", name="about")
+     */
+    public function aboutAction()
+    {
+        return $this->render("@BloggerBlog/Page/about.html.twig");
+    }
+}
