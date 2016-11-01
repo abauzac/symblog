90a9217c117ba21b14c6ef54ccd3169662670ac3
amzac <amaury.bauzac@gmail.com>
2016-11-01 10:25:16 +0100

    Added assetic

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
+            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Blogger\BlogBundle\BloggerBlogBundle()
         ];
 
